9edf1d213e1b81438298b8e01d5a1a3768ce0af3
庞宁 <pangning03@meituan.com>
2021-06-28 20:42:52 +0800

    DEV start 2021/06.28

---
+++ yooneep.leetcode/No_0001_Two_Sum.java
@@ -0,0 +1,82 @@
+
+
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * 你可以按任意顺序返回答案。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ * <p>
+ * 提示：
+ * <p>
+ * 2 <= nums.length <= 104
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * 只会存在一个有效答案
+ * 进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？
+ */
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Project_Name leetcode
+ * @Package_Name PACKAGE_NAME
+ * @Class_Name No1TwoSum
+ * @Description No1TwoSum
+ * @Author pangning
+ * @Date 2021/6/28 8:04 下午
+ * @Version 1.0
+ */
+public class No_0001_Two_Sum {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int[] ints = getTwoSum(nums, 4);
+        System.out.println(Arrays.toString(ints));
+
+        int[] ints1 = getTwoSum1(nums, 4);
+        System.out.println(Arrays.toString(ints1));
+
+    }
+
+    public static int[] getTwoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    public static int[] getTwoSum1(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
